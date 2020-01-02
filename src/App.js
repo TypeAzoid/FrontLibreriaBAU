ed90f4a68e88a9a5b5a3ecb581f6cd540ef74ca5
garodriguez <garodriguez@baufest.com>
2020-01-02 14:41:30 -0300

    router reparado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from "./component/home/home";
 import Clientes from "./component/clientes/clientes";
 import './App.css';
 import Menu from './component/menu/menu';
+import NF404 from './component/404NF/404NF';
 
-function App() {
-  return (
-    <Router>
-    <div>
-      <header>
-        <Menu/>
-      </header>
-        <Route path="/" component={Home} />
-        <Route path="/clientes" component={Clientes} />
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <Menu/>
+        </header>
+        <BrowserRouter>
+            <Switch>
+              <Route
+                path="/home"
+                component={Home} />
+              <Route
+                path="/clientes"
+                render={() => <Clientes name="Clientes" />} />
+              <Route component={NF404}/>
+            </Switch>
+        </BrowserRouter>
+      </React.Fragment>
+    );
+  }
 }
-
 export default App;
---
+++ src/component/404NF/404NF.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class NF404 extends React.Component {
+    render() {
+        return(<h1>418: El servidor se niega a hacer cafe por que es una tetera</h1>);
+    }
+}
+
+export default NF404;

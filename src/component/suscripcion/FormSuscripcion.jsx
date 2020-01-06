77a36fdf12220564e72c5083637dbd529d52bd93
garodriguez <garodriguez@baufest.com>
2020-01-06 14:49:06 -0300

    Suscripcion vista agregar terminada, funcionalidad obtener datos form terminada, falta enviar datos a la api

--- src/component/suscripcion/FormSuscripcion.jsx
+++ src/component/suscripcion/FormSuscripcion.jsx
@@ -15,6 +15,9 @@ class FormSuscripcion extends React.Component {
             buscador: "",
             idc: "-",
             idp: "-",
+            cantidad: "",
+            inicio: "",
+            fin: "",
         }
         this.changeIdc = this.changeIdc.bind(this);
         this.changeIdp = this.changeIdp.bind(this);
@@ -56,6 +59,18 @@ class FormSuscripcion extends React.Component {
         this.setState({buscador: e.target.value});
     }
 
+    cantidadChange = (e) => {
+        this.setState({cantidad: e.target.value});
+    }
+
+    inicioChange = (e) => {
+        this.setState({inicio: e.target.value});
+    }
+
+    finChange = (e) => {
+        this.setState({fin: e.target.value});
+    }
+
     changeBusq = (e) => {
         this.setState({busqueda: e.target.value});
     }
@@ -78,27 +93,51 @@ class FormSuscripcion extends React.Component {
         setInterval(() => this.selectDisplay(this.state.busqueda),500);
     }
 
+    enviar() {
+
+    }
+
+    undisplay() {
+        var elemento = document.getElementById("BSE");
+        var elemento2 = document.getElementById("CSE");
+        elemento.style.display = "none";
+        elemento2.style.display = "none";
+    }
+
+    display() {
+        var elemento = document.getElementById("BSE");
+        var elemento2 = document.getElementById("CSE");
+        elemento.style.display = "block";
+        elemento2.style.display = "block";
+    }
+
     render() {
         return(
             <React.Fragment>
-                <div className="backgroundSuscripcionE"></div>
-                <div className="SuscripcionE">
+                <div className="backgroundSuscripcionE" id="BSE"></div>
+                <div className="SuscripcionE" id="CSE">
                     <div className="Selection">Id cliente  seleccionado: {this.state.idc}</div>
                     <div className="Selection">Id producto seleccionado: {this.state.idp}</div>
-                    Inicio <input type="date" id="Test_Date"></input>
-                    Fin <input type="date" id="Test_Date"></input>
-                    <input type="number" placeholder="cantidad" min="1"></input>
+                    <div className="datecontainer">
+                        Inicio <input type="date" className="Date" value={this.state.inicio} onChange={this.inicioChange}></input>
+                    </div>
+                    <div className="datecontainer">
+                        Fin <input type="date" className="Date" value={this.state.fin} onChange={this.finChange}></input>
+                    </div>
+                    <input type="number" placeholder="cantidad" min="1" value={this.state.cantidad} onChange={this.cantidadChange} className="cantidad"></input>
                     <select className="SelectSuscripcion" value={this.state.busqueda} onChange={this.changeBusq}>
                         <option value="0">Cliente</option>
                         <option value="1">Producto</option>
                     </select>
-                    <input type="text" classname="inputNombre" placeholder="<=== Buscar por" value={this.state.buscador} onChange={this.busChange}></input>
+                    <input type="text" className="inputNombre" placeholder="Buscar" value={this.state.buscador} onChange={this.busChange}></input>
                     <div className="SelectContainer">
                         <div id="showerSCL"><SuscripcionClienteList listado={this.state.clientes} 
                                                                     idc={this.changeIdc}/></div>
                         <div id="showerSPL"><SuscripcionProductoList listado={this.state.productos}
                                                                      idp={this.changeIdp}/></div>
                     </div>
+                    <button className="botonEnviar" onClick={() => this.enviar()}>Enviar</button>
+                    <button className="botonCancelar" onClick={() => this.undisplay()}>Cancelar</button>
                 </div>
             </React.Fragment>
         );
--- src/service/SuscripcionService.jsx
+++ src/service/SuscripcionService.jsx
@@ -7,6 +7,14 @@ class SuscripcionService extends React.Component {
         return axios.get("http://localhost:8080/api/v1/suscripcion/");
     }
 
+    obtenerClientePorId(id) {
+        return axios.get("http://localhost:8080/api/v1/cliente/" + id)
+    }
+
+    obtenerProductoPorId(id) {
+        return axios.get("http://localhost:8080/api/v1/producto/" + id)
+    }
+
 }
 
 export default new SuscripcionService();

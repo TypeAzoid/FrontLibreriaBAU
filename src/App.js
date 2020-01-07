25cf1b40e13d185caa53a490813cca09e52acbe3
Camilo <Lestani>
2020-01-07 10:46:52 -0300

    crud de producto

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./component/home/home";
 import Clientes from "./component/clientes/clientes";
-import './App.css';
-import Menu from './component/menu/menu';
-import NF404 from './component/404NF/404NF';
+import Producto from "./component/producto/Producto";
+import "./App.css";
+import Menu from "./component/menu/menu";
+import NF404 from "./component/404NF/404NF";
+import FormProducto from "./component/producto/FormProducto";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <header>
-          <Menu/>
+          <Menu />
         </header>
         <BrowserRouter>
-            <Switch>
-              <Route
-                path="/home"
-                component={Home} />
-              <Route
-                path="/clientes"
-                render={() => <Clientes name="Clientes" />} />
-              <Route component={NF404}/>
-            </Switch>
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route
+              path="/clientes"
+              render={() => <Clientes name="Clientes" />}
+            />
+            <Route exact path="/producto" component={Producto} />
+            <Route path="/producto/formulario/:id" component={FormProducto} />
+            <Route component={NF404} />
+          </Switch>
         </BrowserRouter>
       </React.Fragment>
     );
--- src/component/menu/menu.jsx
+++ src/component/menu/menu.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import './menu.css';
+import React from "react";
+import "./menu.css";
 
 class Menu extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            loged: 0,
-        }
-    }
-    render() {
-        return(
-            <React.Fragment>
-                <ul id="menu" className="menu">
-                    <li id="homeB"><a href="/home">Home</a></li>
-                    <li id="clientesB"><a href="/clientes">Clientes</a></li>
-                </ul>
-            </React.Fragment>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      loged: 0
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <ul id="menu" className="menu">
+          <li id="homeB">
+            <a href="/home">Home</a>
+          </li>
+          <li id="clientesB">
+            <a href="/clientes">Clientes</a>
+          </li>
+          <li id="productoB">
+            <a href="/producto">Producto</a>
+          </li>
+        </ul>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Menu;
